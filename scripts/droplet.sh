8e20c1eb1265ddbfe49e4b7f13debd6464a80286
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-12-25 14:32:59 +0530

    Update droplet.sh

--- scripts/droplet.sh
+++ scripts/droplet.sh
@@ -30,7 +30,7 @@ echo "................................"
 
 echo "Installing mongodb and nginx"
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt update
 
 # Temporary fix for mongodb
